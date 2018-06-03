4d6f6f73bf89e8446ca930885bb07f8e79b8c073
Mike Granby <mikeg@mikeg.net>
2018-06-03 13:27:22 -0400

    Updated MINGW32 binaries to include GDB.

---
+++ mk-linux.sh
@@ -0,0 +1,3 @@
+name=gcgccc-6.2.0-x86-64-linux-gnu.tar.gz
+rm $name
+tar -C ../gcc/install -cvzf $name .
---
+++ mk-mingw.sh
@@ -0,0 +1,3 @@
+name=gcgccc-6.2.0-i686-w64-mingw32.tar.gz
+rm $name
+tar -C ../gcc/install -cvzf $name .
